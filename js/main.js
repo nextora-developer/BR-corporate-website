97757336cf763ddb4175284d0e1494da67b61636
mingxuanyap <mingxuanyap622@gmail.com>
2025-11-30 01:10:13 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").addClass("position-fixed bg-dark shadow-sm");
+    } else {
+      $(".navbar").removeClass("position-fixed bg-dark shadow-sm");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    loop: true,
+    nav: false,
+    dots: true,
+    items: 1,
+    dotsData: true,
+  });
+})(jQuery);
+
+$(".portfolio-carousel").owlCarousel({
+  autoplay: true,
+  smartSpeed: 900,
+  margin: 25,
+  dots: true,
+  loop: true,
+  center: true,
+  responsive: {
+    0: { items: 1 },
+    576: { items: 1 },
+    768: { items: 2 },
+    992: { items: 3 },
+  },
+});
